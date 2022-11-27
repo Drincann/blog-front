3a10fb9bef19db23684c0359dedfc988eb14f52c
Drincann <1019933576@qq.com>
2022-11-27 23:44:06 +0800

    feat: 调整文章列表中时间的位置

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -44,8 +44,8 @@ const Item = ({ data, onClick }: { data: ArticleDuck, onClick: () => void }) => 
   return (
     <div className="articles-item" onClick={onClick}>
       <div style={{ color: '#fff', fontSize: '1.5rem' }}>{limit(data.title, 50)}</div>
-      <span style={{ float: 'right', color: '#666' }}>{formatDate(data.updateAt)}</span>
-      <p style={{ color: '#999', fontStyle: 'italic', padding: '0 0.5rem', fontSize: '0.5rem' }}>{limit(data.content, 100)}</p>
+      <p style={{ color: '#999', fontStyle: 'italic', padding: '0 0.3rem', fontSize: '0.5rem' }}>{limit(data.content, 100)}</p>
+      <span style={{ float: 'right', color: '#666', backgroundColor: '#111' }}>{formatDate(data.updateAt)}</span>
     </div>
   )
 }
