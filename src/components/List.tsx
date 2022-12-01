6ed1d62406bfd813a0891251cbc3ddeb2c860e10
Drincann <1019933576@qq.com>
2022-12-01 19:10:29 +0800

    feat: 接口变动, 为节省带宽, 在需要时请求 article.content

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { Cover } from "./Article";
 import './List.css'
 import { ArticleDuck } from "../types/ducks";
@@ -12,14 +12,29 @@ interface ListProps {
 }
 
 export const List = ({ filter }: ListProps) => {
+  const { current: articlesContentCache } = useRef<{ [_id: string]: string }>({});
   const [articles, setArticles] = useState<ArticleDuck[]>([]);
   useEffect(
     () => {
-      api.call('getArticles', filter).then(res => setArticles(res.data));
+      api.call('getArticles', { ...filter, addBrief: true, pick: ['_id', 'title', 'createAt', 'updateAt',] }).then(res => setArticles(res.data));
     }, [filter]);
   return (
     <div className="articles-root">
-      {articles.map((article) => <Item key={article._id} data={article} onClick={() => Cover.open(article.content)} />)}
+      {articles.map((article) => <Item key={article._id} data={article} onClick={() => {
+        if (articlesContentCache[article._id]) {
+          Cover.open(articlesContentCache[article._id])
+        } else {
+          api.call('getArticles', { _id: article._id, pick: ['content'] }).then(res => {
+            const article = res.data?.[0];
+            if (article) {
+              Cover.open(article?.content);
+            } else {
+              Cover.open('# Not Found');
+            }
+          })
+        }
+      }
+      } />)}
     </div>
   )
 };
@@ -37,7 +52,13 @@ const Item = ({ data, onClick }: { data: ArticleDuck, onClick: () => void }) => 
   return (
     <div className="articles-item" onClick={onClick}>
       <div className="articles-item-title">{limit(data.title, 50)}</div>
-      <p className="articles-item-content">{limit(data.content, 100)}</p>
+      <p className="articles-item-content">{limit(data.brief, 99)
+      /**
+       * 列表默认请求 content 摘要, 后端会返回长度为 100 的 brief,
+       * limit 的行为在超过给定长度时会截断并在末尾添加 '...', 所以这
+       * 里若填写 100 则一定不会显示 '...', 若填写 99 则保证省略号的
+       * 出现时机是一定正确的。
+       */}</p>
       <div className="articles-item-time">
         <span className="articles-item-time-item" style={{ marginRight: '0.5rem' }}>{formatDate(data.updateAt)}</span>
         <span className="articles-item-time-item" style={{ marginRight: '0.5rem' }}>/</span>
